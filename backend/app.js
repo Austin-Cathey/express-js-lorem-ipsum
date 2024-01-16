5d9908dd890345088330e81ae0cc76818b02ca58
Austin-Cathey <austincatheyart@gmail.com>
2024-01-16 10:34:13 -0500

    works! randomly generates Lorem Ipsum

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,17 @@
 const express = require('express')
+/* import { LoremIpsum } from "lorem-ipsum"; */
+const LoremIpsum = require("lorem-ipsum").LoremIpsum;
+
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 8,
+    min: 4
+  },
+  wordsPerSentence: {
+    max: 16,
+    min: 4
+  }
+});
 
 const app = express()
 const cors = require('cors')
@@ -8,7 +21,9 @@ const config = { port: process.env.PORT || 3000 }
 app.use(cors())
 
 // your API route(s) here
-
+app.get("/lorem/", (req, res) => {
+  res.json({lorem: lorem.generateSentences(7)})
+})
 
 app.get('*', function (req, res) {
   res.status(404).json({ error: 'route not found' })
