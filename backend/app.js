8891831c6075a139965041f53816d8ce2b13fda3
Austin-Cathey <austincatheyart@gmail.com>
2024-01-18 13:39:53 -0500

    :numberOfParagraphs works!

--- backend/app.js
+++ backend/app.js
@@ -25,18 +25,15 @@ app.get("/lorem/", (req, res) => {
   res.json({lorem: `lorem: ${lorem.generateSentences(2)}`})
 }
 )
+
 app.get("/lorem/:number", (req, res) => {
-  res.json({lorem: lorem.generateParagraphs(req.params.number)})
+  res.json({lorem: lorem.generateParagraphs(parseInt(req.params.number))})
 })
 
 app.get('*', function (req, res) {
   res.status(404).json({ error: 'route not found' })
 })
 
-
-/* app.get("/lorem/:2", (req, res) => {
-  res.json({lorem: lorem.generateParagraphs(req.params.number)})
-}) */
 // start server
 app.listen(config.port, () => {
   console.log(`App listening at http://localhost:${config.port}`)
